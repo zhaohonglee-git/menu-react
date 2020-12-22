2f5dca0958fc4af91efb19165b3992406171196e
zhaohonglee <296222105@qq.com>
2020-12-22 22:13:09 +0800

    last commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import items from './data'
 import Categories from './components/Categories.jsx'
 import Menu from './components/Menu'
 
+const allCategories = [...new Set(items.map(item => item.category)), 'all']
+console.log(allCategories, '所有JSON对象中的categories,需要用new Set方法进行去重,还需要把all包含进去')
+
 function App() {
   const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+
+  // 过滤菜单
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
 
   return (
     <main>
       <section className='menu section'>
         <div className='title'>
-          <h2>美食美刻菜单</h2>
+          <h2>美食美刻--菜单</h2>
           <div className='underline'></div>
         </div>
-        <Categories></Categories>
+        <Categories filterItems={filterItems} categories={categories} ></Categories>
         <Menu items={menuItems}></Menu>
       </section>
     </main>
-
   );
 }
 
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const Categories = () => {
-  return (
-    <div>
+const Categories = ({ filterItems, categories }) => {
+  console.log(categories, '打印测试数组的显示')
 
+  return (
+    <div className='btn-container'>
+      {categories.map((item, index) => {
+        return (
+          <button className='filter-btn' key={index} onClick={() => filterItems(item)}>
+            {item}
+          </button>
+        )
+      })}
     </div>
   )
 }
